ee6bece0f08e4838a732ae6bdb02d96d03cbddf9
SahilSonekar <sahilsonekar25@gmail.com>
2025-11-19 11:47:54 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"  // Treat all js code as newer version
+
+//alert(3+3)  // we are usingh m nodejs, not browser
+
+console.log(3
+    +
+    3);     // code readability should be high 
+
+console.log("Sahil")
+
+let Name = "Sahil"
+let age = 18
+let isLoggedIn = true
+let State;
+
+//premetive dataTypes 
+
+// Number => 2 to the power 52
+// bigint
+// String
+// boolean => true, false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined);
