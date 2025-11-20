44a0639cfafd4d79597065f0987f56ad23106ee1
SahilSonekar <sahilsonekar25@gmail.com>
2025-11-20 12:16:08 +0000

    Stack and heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,10 +12,10 @@ let userEmail;
 const id = Symbol("123")
 const anotherId = Symbol("123")
 
-console.log(id == anotherId);
+// console.log(id == anotherId);
 
 const bigNumber = 12356556523225332n
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 
 
 // Reference (Non-primitive)
@@ -32,4 +32,34 @@ const myFunction = function(){
     console.log("Hello World")
 }
 
-console.log(typeof anotherId)
+// console.log(typeof anotherId)
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive), Heap(Non-Primitive)
+
+let myYoutubename = "SahilSonekar"
+
+let anothername = myYoutubename
+anothername = "ChaiaurCode"
+
+// console.log(myYoutubename)
+// console.log(anothername)
+
+let userOne ={
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "Sahil@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+// In stack the value assigned to a variable or anything is a copy
+// In Heap the value assigned to a variable or anything is the direct reference(Original Value)
+
+// in userOne.email when we changed the value of userTwo.email the value of userOne.email(Main) also changed 
