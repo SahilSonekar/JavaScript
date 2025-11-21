e9e41b664ec95bb4f61d8e8e7e38f5e3146f64a2
SahilSonekar <sahilsonekar25@gmail.com>
2025-11-21 12:18:21 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+const name = "Sahil"
+const repoCount = 50
+
+// console.log(name +  repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName =new String('Sahil-Sonekar-com')
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf(' '));
+
+// const newString = gameName.substring(0,4)       // Cannot give negative index
+// console.log(newString);
+
+// const anotherString = gameName.slice(-10, 7)
+// console.log(anotherString);
+
+const newStringOne = '   Sahil   '
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = 'http://sahil.com//sahil%20sonekar'
+// console.log(url.replace('%20', '-'))
+// console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
+
