dc7d75916479a77217803709d5f3dfde7c7a06a6
SahilSonekar <sahilsonekar25@gmail.com>
2025-11-28 13:33:44 +0000

    While and do while loop

---
+++ 05_iterations/While_and_DoWhile.js
@@ -0,0 +1,26 @@
+// While Loop 
+
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index += 2
+}
+
+
+let myArray = ["Iron man", "Batman", "Superman"]
+let arr = 0
+
+while (arr < myArray.length) {
+    // console.log(`Your super hero is ${myArray[arr]}`);
+    arr += 1
+}
+
+
+// Do while loop 
+
+let score = 0
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
